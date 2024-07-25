c7de8576b82098d6e2e52c5cebfccb1f50036242
saurav4r <saurav4ryou707997@gmail.com>
2024-07-25 19:49:57 +0530

    somechanges

--- src/Home/components/Hyper.tsx
+++ src/Home/components/Hyper.tsx
@@ -17,17 +17,33 @@ import IssueTracker from "./Issue";
 import ChatBox from "./Chat";
 import PomodoroTimer from "./Pomodore";
 import Youtubecc from "./youtubecard";
+import back from '../image/back.jpeg'
 
 export default function Hyper() {
-  return (
-    <div className="flex flex-col">
-      <div className="p-8">
-        <h1 className="text-center font-black text-2xl pt-6 pb-3 font-sans">
-          Welcome To Hyperverge chrome Extension
-        </h1>
-      </div>
 
-      <div className="flex flex-row p-8">
+  const backgroundImageStyle = {
+    backgroundImage: `url(${back})`,
+    backgroundSize: 'cover', // This will cover the entire component
+    backgroundPosition: 'center', // This will center the image
+    height: '100vh', // Set the height of the component
+    width: '100%', // Set the width of the component
+    
+  };
+
+  return (
+    <div className="flex flex-col" >
+      <div className="p-8 ">
+  <h1 className="text-center text-white text-3xl font-bold pt-6 pb-3 font-sans tracking-wide shadow-2xl">
+    Welcome To Hyperverge Chrome Extension
+  </h1>
+  <h3 className="text-center text-white text-xl font-medium pt-6 pb-3 font-sans tracking-wide shadow-md">
+    HyperVerge Dominates APAC Market with AI-Powered Identity Verification, Serving 700 Million Identities Globally
+  </h3>
+</div>
+
+
+
+      <div className="flex flex-row p-8" >
 
         <div className="w-1/2">
             <div className="pt-4 pb-6">
--- src/Home/newtab.tsx
+++ src/Home/newtab.tsx
@@ -2,16 +2,26 @@ import React, { useEffect } from "react";
 import * as ReactDOMClient from "react-dom/client";
 import "../styles/global.css";
 import Hyper from "./components/Hyper";
+import back from "./image/back.jpeg";
 
 const App: React.FC<{}> = () => {
+
+  const backgroundImageStyle = {
+    backgroundImage: `url(${back})`,
+    backgroundSize: 'cover', // This will cover the entire component
+    backgroundPosition: 'center', // This will center the image
+    backgroundAttachment: 'fixed', // This will make the background image stick
+    height: '100vh', // Set the height of the component
+    width: '100%', // Set the width of the component
+    overflow: 'auto', // Ensure content can overflow
+  };
+
   return (
-   
+    <div style={backgroundImageStyle}>
       <div>
-        <div>
-            <Hyper/>
-        </div>
+        <Hyper />
       </div>
-    
+    </div>
   );
 };
 
@@ -22,7 +32,7 @@ document.body.appendChild(rootElement);
 const root = ReactDOMClient.createRoot(rootElement);
 
 root.render(
- 
-    <App />
-   
+  
+  <App />
+
 );
